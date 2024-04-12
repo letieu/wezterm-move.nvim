738b12f98bf21ddaa8400c5d56a8fc3b916ff99b
letieu <letieu8@gmail.com>
2024-04-12 12:48:04 +0700

    init

---
+++ lua/wezterm-move/init.lua
@@ -0,0 +1,35 @@
+local WM = {}
+
+-- @param direction: string (h, j, k, l)
+local function at_edge(direction)
+  return vim.fn.winnr() == vim.fn.winnr(direction)
+end
+
+local function wezterm_exec(cmd)
+  local command = vim.deepcopy(cmd)
+  table.insert(command, 1, "wezterm")
+  table.insert(command, 2, "cli")
+  return vim.fn.system(command)
+end
+
+local wezterm_directions = {
+  h = "Left",
+  j = "Down",
+  k = "Up",
+  l = "Right",
+}
+
+-- @param direction: string (h, j, k, l)
+local function send_key_to_wezterm(direction)
+  wezterm_exec({ "activate-pane-direction", wezterm_directions[direction] })
+end
+
+WM.move = function(direction)
+  if at_edge(direction) then
+    send_key_to_wezterm(direction)
+  else
+    vim.cmd("wincmd " .. direction)
+  end
+end
+
+return WM
